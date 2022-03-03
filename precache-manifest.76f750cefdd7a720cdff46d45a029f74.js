b7174f70ce703c682a706d51bfbbc59baee293ff
Areej Sultana <82303448+areej-sultana1@users.noreply.github.com>
2022-03-03 22:09:24 +0500

    Add files via upload

---
+++ precache-manifest.76f750cefdd7a720cdff46d45a029f74.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a162e4b0d9a55bf230",
+    "url": "/Game-MissingNumber/static/css/main.0c12fe89.chunk.css"
+  },
+  {
+    "revision": "83a162e4b0d9a55bf230",
+    "url": "/Game-MissingNumber/static/js/main.be7a28ca.chunk.js"
+  },
+  {
+    "revision": "3df6deecd9565fe2c56b",
+    "url": "/Game-MissingNumber/static/js/runtime~main.3b372bd6.js"
+  },
+  {
+    "revision": "5bd9d4bd72372dc1f2b5",
+    "url": "/Game-MissingNumber/static/css/2.2ae034a2.chunk.css"
+  },
+  {
+    "revision": "5bd9d4bd72372dc1f2b5",
+    "url": "/Game-MissingNumber/static/js/2.eaf0cce1.chunk.js"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/Game-MissingNumber/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/Game-MissingNumber/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/Game-MissingNumber/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/Game-MissingNumber/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/Game-MissingNumber/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "efc0e3314bf0a9623a140aa5e147db9f",
+    "url": "/Game-MissingNumber/static/media/background.efc0e331.gif"
+  },
+  {
+    "revision": "669f8cdb6aa1730b188e1d564f50d648",
+    "url": "/Game-MissingNumber/static/media/bar.669f8cdb.png"
+  },
+  {
+    "revision": "74c56db6bf4aca01f7bc3e1699fa7976",
+    "url": "/Game-MissingNumber/index.html"
+  }
+];
